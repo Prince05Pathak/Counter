83639a8eb8ab1d0b113f0c9131a708d8896d19c8
Prince05Pathak <102548615+Prince05Pathak@users.noreply.github.com>
2025-01-14 13:25:10 +0530

    Add files via upload

---
+++ FirstApp.dart
@@ -0,0 +1,148 @@
+import 'dart:io'; // This is to take input and show output in dart language
+
+void main() {
+  print('Hello World');
+
+//   //stdout.write('Enter your name:');
+//
+//   //var name = stdin.readLineSync();
+//
+//   //print("Welcome,$name ");
+//
+//   //var prince = human(); // Creating a class object without using the new keyword
+//
+// // Variables ->  Are the instances that are used to identify the data types and objects in the program
+//
+//   //inline declaration
+//   //String name2 = "RAMAN";
+//   //stdout.write(name2);
+//
+//   //int num = 12345678910111213141516;  This will not work
+//   //print(num);
+//   //Bigint
+//   //BigInt longValue = BigInt.parse('12345678910111213141516');
+//   //stdout.write(longValue);
+//
+//   // Dynamic data type are the data type which can be overwrite at the point of runtime
+//
+//   //var section;
+//
+//   //section = "D";
+//   //stdout.write(section);
+//
+//   //section = 24;
+//   //stdout.write(section);
+//
+//   // Functions in Dart
+//   var myC =
+//   myClass(); // Creating the class object / instance to initialize the class
+//   myC.printName("Prince Pathak "); // Function Calling
+//
+//   //stdout.write('Enter your first number: ');
+//   int num01 = 5; //int.parse(stdin.readLineSync()!);
+//
+//   //stdout.write('Enter your second number: ');
+//   int num02 = 5; //int.parse(stdin.readLineSync()!);
+//
+//   int result = myC.sumNum(num01, num02);
+//   stdout.write(result.toString() + "\n");
+//
+//   // List in dart
+//
+//   var listNumbers = [1, 2, 3, 4, 5];
+//   listNumbers.add(50);
+//   print(listNumbers);
+//
+//   var names = [];
+//   //numbers.addAll(listNames);
+//   names.add("Prince");
+//   names.add("Prashant");
+//   names.add("Piyush");
+//   names.add("Paras");
+//   names.insert(3, 100);
+//
+//   names.insertAll(2, listNumbers);
+//   print(names);
+//   //---------------------------------------------------------------------------
+//
+//
+//   // MAPS IN DART
+//
+//   stdout.write("This is the about map's  \n");
+//   // Maps are dictionary-like data types that exist in key value pair
+//   // var map_name = {key01:Value01, key02:Value02};
+//   //It is important to note that all keys need to be unique and the keys are case sensitive
+//   var map_name = {
+//     'Key01':'Value1',
+//     'Key02':45,
+//     'Key03':true
+//   };
+//
+//   stdout.write(map_name.toString()+'\n');
+//   map_name['Key01'] = 'Prince';
+//   stdout.write("This shows the overwriten value "+map_name.toString()+'\n');
+//   stdout.write("The key 2 is "+map_name['Key02'].toString()+ '\n');
+//
+//   var mapName = Map();
+//
+//   mapName['Name'] = "Prince" ;
+//   mapName['Age'] = 22 ;
+//   mapName['D.O.B'] = "05/04/2002" ;
+//
+//
+//   print(mapName);
+//   print(mapName.keys);
+//   print(mapName.isNotEmpty);
+//   print(mapName.isEmpty);
+//   print(mapName.length);
+//   print(mapName.values);
+//   print(mapName.containsKey('Name'));
+//   print(mapName.containsValue(false));
+//   print(mapName.remove('Age'));
+//   print(mapName);
+
+// Conditional Statements in Dart
+//   var a =100;
+//   if(a>200){
+//     //true cond 1
+//   } else if (a<50) {
+//     // Cond 2 is true
+//   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// // Classes and Objects in Dart
+//
+// class human {
+//   human(); // This is the default constructor of the class which is not required ot be defined
+// }
+// class myClass {
+//   myClass() {
+//     // This function is the default constructor
+//     stdout.write("myClass object has been created! \n");
+//   }
+//   void printName(String name) {
+//     // Function Declaration
+//     var name = "Prince";
+//     stdout.write("Welcome Mr." + name + '\n'); // Function Defination
+//   }
+//
+//   int sumNum(number01, number02) {
+//     return number01 + number02;
+//   }
+// }
